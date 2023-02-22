359936f0118d75657ff423638c8fbe0871103afc
ajiprasetyo471 <ajiprsty4713@gmail.com>
2023-02-22 23:06:34 +0700

    finish design

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    class="container-fluid vh-100 d-flex justify-content-center align-items-center"
+  >
+    <DadJokes />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DadJokes from './components/DadJokes.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { DadJokes },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container-fluid {
+  background: #ffb6c1;
 }
 </style>
---
+++ src/components/DadJokes.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card w-50 h-50 py-3 px-3" style="width: 18rem">
+    <div
+      class="card-body d-flex flex-column justify-content-between align-items-end"
+    >
+      <h2 class="card-title">Special title treatment</h2>
+      <h5 class="card-text">
+        With supporting text below as a natural lead-in to additional content.
+        😂
+      </h5>
+      <btn class="btn btn-danger btn-lg">Reload 💫</btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DadJokes',
+};
+</script>
+
+<style lang="scss" scoped>
+.card-body {
+  text-align: right !important;
+}
+
+.card-title {
+  font-weight: 400 !important;
+}
+
+.card-text {
+  font-weight: 600 !important;
+}
+
+.btn {
+  width: 140px !important;
+}
+</style>
