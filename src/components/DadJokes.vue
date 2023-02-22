1ad46644afa0b7ac7aedc69fe714a03616bea7a2
ajiprasetyo471 <ajiprsty4713@gmail.com>
2023-02-22 23:21:50 +0700

    finish integration and validation

--- src/components/DadJokes.vue
+++ src/components/DadJokes.vue
@@ -3,19 +3,44 @@
     <div
       class="card-body d-flex flex-column justify-content-between align-items-end"
     >
-      <h2 class="card-title">Special title treatment</h2>
-      <h5 class="card-text">
-        With supporting text below as a natural lead-in to additional content.
-        😂
+      <h2 class="card-title">{{ setup }}</h2>
+      <h5 v-if="punchline !== ''" class="card-text fw-bold">
+        {{ punchline }} 😂
       </h5>
-      <btn class="btn btn-danger btn-lg">Reload 💫</btn>
+      <btn class="btn btn-danger btn-lg" @click="getDadJokes">Reload 💫</btn>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'DadJokes',
+  data() {
+    return {
+      setup: '',
+      punchline: '',
+    };
+  },
+  mounted() {
+    this.getDadJokes();
+  },
+  methods: {
+    async getDadJokes() {
+      try {
+        let data = await axios({
+          url: 'https://official-joke-api.appspot.com/jokes/random',
+          method: 'GET',
+        });
+        const jokes = data.data;
+        this.setup = jokes.setup;
+        this.punchline = jokes.punchline;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
 
@@ -28,9 +53,9 @@ export default {
   font-weight: 400 !important;
 }
 
-.card-text {
-  font-weight: 600 !important;
-}
+//.card-text {
+//  font-weight: 600 !important;
+//}
 
 .btn {
   width: 140px !important;
